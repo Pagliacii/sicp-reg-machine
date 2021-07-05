94341393158b465d078c188b347671f47f474402
Pagliacii <huangmianrui0310@outlook.com>
2021-07-05 23:34:54 +0800

    feat(WIP): implement make_machine and assemble functions

---
+++ src/machine/assemble.rs
@@ -0,0 +1,6 @@
+use super::machine::Machine;
+
+/// The assemble procedure is the main entry to the assembler.
+/// It takes the controller text and the machine model as arguments
+/// and returns the instruction sequence to be stored in the model.
+pub fn assemble(controller_text: &str, machine: &Machine) {}
--- src/machine/mod.rs
+++ src/machine/mod.rs
@@ -2,9 +2,31 @@ mod errors;
 mod function;
 mod value;
 
-pub mod machine;
-pub mod operation;
-pub mod register;
-pub mod stack;
+mod machine;
+mod operation;
+mod register;
+mod stack;
+
+use std::collections::HashMap;
+
+use errors::Result;
+use machine::Machine;
+use operation::Operation;
 
 type BaseType = std::sync::Arc<dyn std::any::Any + Send + Sync>;
+
+/// Constructs and returns a model of the machine with
+/// the given registers, operations, and controller.
+#[allow(dead_code)]
+pub fn make_machine<F, Args, R>(
+    register_names: Vec<&str>,
+    operations: &HashMap<&str, Operation>,
+    _controller_text: &str,
+) -> Result<Machine> {
+    let mut machine = Machine::new();
+    for &reg_name in register_names.iter() {
+        machine.allocate_register(reg_name)?;
+    }
+    machine.install_operations(operations);
+    Ok(machine)
+}
