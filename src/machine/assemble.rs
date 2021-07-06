1c894df5aee417fbb1e0947c54eb65b2ad531f3d
Pagliacii <huangmianrui0310@outlook.com>
2021-07-06 14:38:27 +0800

    feat: implement the assemble function

--- src/machine/assemble.rs
+++ src/machine/assemble.rs
@@ -1,6 +1,42 @@
-use super::machine::Machine;
+use std::collections::HashMap;
+
+use crate::parser::{parse, RMLNode};
 
 /// The assemble procedure is the main entry to the assembler.
-/// It takes the controller text and the machine model as arguments
-/// and returns the instruction sequence to be stored in the model.
-pub fn assemble(controller_text: &str, machine: &Machine) {}
+pub fn assemble(
+    controller_text: &str,
+) -> Result<(Vec<RMLNode>, HashMap<String, Vec<RMLNode>>), String> {
+    let mut labels: HashMap<String, Vec<RMLNode>> = HashMap::new();
+    let mut insts: Vec<RMLNode> = vec![];
+    let parse_result = parse(controller_text).unwrap();
+    let mut current_label = String::from("");
+
+    // Split the parse result into instructions set and label-insts map.
+    for node in parse_result.into_iter() {
+        match node {
+            RMLNode::Symbol(label) => {
+                if labels.contains_key(&label) {
+                    return Err(format!("[ASSEMBLE] Duplicated label: {}", label));
+                }
+                if !insts.is_empty() {
+                    // Instructions start with a control label are edge case.
+                    // If without checking empty, the result will lost the execute order.
+                    current_label = label;
+                }
+                labels.insert(label, vec![]);
+            }
+            inst => {
+                if current_label == "" {
+                    insts.push(inst);
+                } else {
+                    if let Some(&mut v) = labels.get_mut(&current_label) {
+                        v.push(inst);
+                    } else {
+                        return Err(format!("[ASSEMBLE] Missed label: {}", current_label));
+                    }
+                }
+            }
+        }
+    }
+    Ok((insts, labels))
+}
