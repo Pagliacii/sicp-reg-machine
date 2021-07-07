8e4c7188351239ad473c80e59ed9500ece31e7d5
Pagliacii <huangmianrui0310@outlook.com>
2021-07-07 21:54:20 +0800

    feat: add a gcd machine

---
+++ src/bin/gcd.rs
@@ -0,0 +1,44 @@
+use std::collections::HashMap;
+
+use reg_machine::{
+    machine::{
+        operation::Operation,
+        value::{FromValue, Value},
+        Operations,
+    },
+    make_machine,
+};
+
+const CONTROLLER_TEXT: &str = r#"
+(controller
+ test-b
+   (test (op =) (reg b) (const 0))
+   (branch (label gcd-done))
+   (assign t (op rem) (reg a) (reg b))
+   (assign a (reg b))
+   (assign b (reg t))
+   (goto (label test-b))
+ gcd-done)
+"#;
+
+fn operations() -> Operations {
+    let mut operations: Operations = HashMap::new();
+    operations.insert("=", Operation::new(|b: i32| 0 == b));
+    operations.insert("rem", Operation::new(|a: i32, b: i32| a % b));
+    operations
+}
+
+fn main() {
+    let register_names = vec!["a", "b", "t"];
+    let operations = operations();
+    let mut machine = make_machine(register_names, &operations, &CONTROLLER_TEXT).unwrap();
+    machine
+        .set_register_content("a", Value::Integer(1023))
+        .unwrap();
+    machine
+        .set_register_content("b", Value::Integer(27))
+        .unwrap();
+    assert_eq!(Ok("Done"), machine.start());
+    let value = machine.get_register_content("a").unwrap();
+    println!("gcd(1023, 27) = {}", i32::from_value(value).unwrap());
+}
