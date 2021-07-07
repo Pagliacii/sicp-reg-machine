71fd4cd72a59a9262718015471c8ac17bf5a4b41
Pagliacii <huangmianrui0310@outlook.com>
2021-07-07 17:10:23 +0800

    fix: fix instructions lost after assembling insts x2

--- src/assemble.rs
+++ src/assemble.rs
@@ -9,9 +9,6 @@ pub fn assemble(
     let mut labels: HashMap<String, Vec<RMLNode>> = HashMap::new();
     let mut insts: Vec<RMLNode> = vec![];
     let parse_result = parse(controller_text).unwrap();
-    let mut current_label = String::from("");
-    let mut leading_label: Option<String> = None;
-    let mut first_inst = true;
 
     // Split the parse result into instructions set and label-insts map.
     for node in parse_result.into_iter() {
@@ -20,25 +17,15 @@ pub fn assemble(
                 if labels.contains_key(&label) {
                     return Err(format!("[ASSEMBLE] Duplicated label: {}", label));
                 }
-                if first_inst {
-                    leading_label = Some(label.clone());
-                }
-                first_inst = false;
-                current_label = label.clone();
                 labels.insert(label, vec![]);
             }
             inst => {
-                insts.push(inst.clone());
-                if let Some(v) = labels.get_mut(&current_label) {
-                    v.push(inst);
-                } else {
-                    return Err(format!("[ASSEMBLE] Missed label: {}", current_label));
+                for (_, val) in labels.iter_mut() {
+                    val.push(inst.clone());
                 }
+                insts.push(inst);
             }
         }
-    }
-    if let Some(label) = leading_label {
-        labels.insert(label, insts.clone());
     }
     Ok((insts, labels))
 }
